ab5f75487283b8c42c8b63b9131ac76b09235864
Anastasia Novosyolova <anastasia@Anastasias-MacBook-Air.local>
2022-08-31 18:44:17 +0300

    fixed

--- homework-03/homework-03.js
+++ homework-03/homework-03.js
@@ -1,35 +1,34 @@
 // 1
 function getMaxDigit(number){
-    const int = String(number);
-    return Math.max(...int)
-};
+    const num = String(number);
+    return Math.max(...num)
+}
+
 
 // 3
 function makeName(str) {
     return str[0].toUpperCase() + str.slice(1).toLowerCase();
-};
-
+}
 
 
 // 4 
-function countSum (salary) {
+function countSum(salary) {
    const tax1 = 18;
    const tax2 = 1.5;
    const taxes = tax1 + tax2;
    const taxSum = salary / 100 * taxes
    return salary - taxSum;
-};
+}
 
 
 // 5
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
-};
-
+}
 
 
 // 7
-function converter (str) {
+function converter(str) {
     let rate = 25;
 
     if (str.includes('$')) {
@@ -38,17 +37,19 @@ function converter (str) {
 
     if (str.toUpperCase().includes('UAH')) {
        return Number(str.slice(0, -3)) / rate + '$'
-    } else {
-       return 'Error';
     };
-};
+    return 'Error';
+}
 
 
  // 8
 function getRandomPassword(length = 8) {
-    return Math.floor(Math.pow(10, length-1) + Math.random() * (Math.pow(10, length) - Math.pow(10, length-1) - 1));
-};
+  
+    let max = Math.pow(10, length - 1);
+    let min = Math.pow(10, length) - Math.pow(10, length - 1);
 
+    return Math.floor(max - Math.random() * min + min);
+}
 
 
 document.writeln(`Функція №1: ${getMaxDigit(348534)} <br>`);
@@ -56,4 +57,4 @@ document.writeln(`Функція №3: ${makeName('bubBaSmith')} <br>`);
 document.writeln(`Функція №4: ${(countSum(1000))} <br>`);
 document.writeln(`Функція №5: ${(getRandomNumber(1, 10))} <br>`);
 document.writeln(`Функція №7: ${(converter('2500uah'))} <br>`);
-document.writeln(`Функція №4: ${(getRandomPassword(4))} <br>`);
+document.writeln(`Функція №4: ${(getRandomPassword(5))} <br>`);
--- homework-04/homework-04.js
+++ homework-04/homework-04.js
@@ -1,33 +1,85 @@
-const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
 
 const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
 
 const marks = [4, 5, 5, 3, 4, 5];
 
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
 
 
-function someFunc (arr) {
-    const girls = [];
-    const boys = [];
-    
-    for (i = 0; i <= arr.length; i++) {
+// function someFunc(arr) {
 
-       if (arr[i].endsWith("а") || arr[i].endsWith("я")) {
-           girls.push(arr[i]);
-       } else {
-           boys.push(arr[i]);
-       };
+//     let girls = []
+//     let boys = []
 
+//     for (let i = 0; i < arr.length; i++) {
+
+//         (arr[i].endsWith("а")) ? girls.push(arr[i]) : boys.push(arr[i])  
+//     }; 
+//   return [boys, girls]
+// };
+
+// console.log(someFunc(students))
+
+
+let girls = []
+let boys = []
+let pairs = []
+
+function someFunc(arr) {
+
+    for (let i = 0; i < arr.length; i++) {
+
+      (arr[i].endsWith("а")) ? girls.push(arr[i]) : boys.push(arr[i]);
+    }; 
+  return [boys, girls]
 };
-}
+
 console.log(someFunc(students))
-// function getPairs(students) {
-//     for (i = 0; i < students.length; i++) {
-//         const girls = i.endsWith('a');
-//     };
-//     console.log(girls)
+
+
+function makePairs(arr1, arr2) {
+
+   for (let i = 0; i < arr1.length; i++) {
+    
+pairs.push(arr1[i]);
+pairs.push(arr2[i]);
+pairs.slice(i, i += 2);
+   }
+   return pairs
+   
+}
+console.log(makePairs(girls, boys))
+
+
+
+
+// function makePairs(arr1, arr2) {
+// const pairs = [];
+
+// pairs.push()
 // }
 
+// const initialArray = [1, 2, 3, 4, 5, 6]
+// const newArr = boys.reduce((result, value, index, array) => {
+//   if (index = 0 ) {
+//     return [...result, array.slice(index, index +1)];
+//   }
+//   return result
+// }, []);
+// console.log(newArr)
+
+// function getPairs(arr) {
+
+//     const girls = [];
+//     const boys = [];
+
+//     for (i = 0; i < arr.length; i++) {
+//       girls.push(arr[i].endsWith('a') && arr[i].endsWith('я'));
+//       boys.push(...arr);
+//     };
+
+// }
+// console.log(getPairs(students))
 
 
 // let txt = "";
