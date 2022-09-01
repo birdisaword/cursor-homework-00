372463340bc9ce0cc5aaddf16648bbf616119c6d
Anastasia Novosyolova <anastasia@Anastasias-MacBook-Air.local>
2022-09-01 20:45:55 +0300

    hw-04 added

--- homework-04/homework-04.js
+++ homework-04/homework-04.js
@@ -6,7 +6,6 @@ const marks = [4, 5, 5, 3, 4, 5];
 const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
 
 
-
 let girls = []
 let boys = []
 let sortedList = []
@@ -14,73 +13,72 @@ const pairs = [];
 const namesAndTheme = [];
 const fullInfo = [];
 
-function someFunc(arr) {
+// two arrays with girls and with boys
 
+function someFunc(arr) {
     for (let i = 0; i < arr.length; i++) {
 
       (arr[i].endsWith("а")) ? girls.push(arr[i]) : boys.push(arr[i]);
-    }; 
+    }
   return [boys, girls]
-};
-
+}
 console.log(someFunc(students))
 
 
-
+// combined array - [girl, boy, g, b, etc]
 
 function filterGroup(arr1, arr2) {
-
     for (let i = 0; i < arr1.length; i++) {
      
  sortedList.push(arr1[i], arr2[i]);
     }
+  return sortedList
 }
-  
  console.log(filterGroup(girls, boys))
-//  console.log(sortedList)
+
  
+// arrays [girl, boy]
+
  function makePairs(arr) {
-     
      for (let i = 0; i < arr.length; i += 2) {
          const sortedList = arr.slice(i, i + 2);
          pairs.push(sortedList);
      }
-     return pairs;
+   return pairs;
  }
  console.log(makePairs(sortedList))
 
 
+// array with pair - girl, boy and their theme
 
  function pairAndTheme(arr1, arr2) {
-   
     for (let i = 0; i < arr1.length; i ++) {
         namesAndTheme.push([arr2[i].join(" і "), String(arr1.slice(i, i + 1))])
     }
-return namesAndTheme;
+  return namesAndTheme;
  }
 console.log(pairAndTheme(themes, pairs))
 
+
+// students and their marks
 
 function marksForStudents(arr1, arr2) {
     const marked = [];
     for (let i = 0; i < arr1.length; i ++) {
         marked.push([arr1[i], arr2[i]])
-}
-return marked
+    }
+  return marked
 }
 console.log(marksForStudents(students, marks))
 
  
+// pair of students, their theme and random mark
 
 function addFullInfo(arr) {
-    
-    
-
     for (let i = 0; i < arr.length; i ++) {
      let randomMark = Math.floor(Math.random()*5 + 1); 
-fullInfo.push([String(arr[i]), String(randomMark)])
+     fullInfo.push([String(arr[i]), String(randomMark)])
     }
-    return fullInfo
+   return fullInfo
 }
-
 console.log(addFullInfo(namesAndTheme))
