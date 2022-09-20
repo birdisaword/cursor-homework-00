49da05a39ee9256a5a9229693e2f8f07fb97912b
Anastasia Novosyolova <anastasia@Anastasias-MacBook-Air.local>
2022-09-21 00:04:23 +0300

    fixed

--- homework-05/homework-05.js
+++ homework-05/homework-05.js
@@ -1,106 +1,109 @@
-// 1
+
+// 1 масив випадкових цілих чисел
 
   function getRandomArray(length, min, max) {
     const numbers = [];
+    
     for (let i = min; i <= length; i++) {
-    numbers.push(Math.floor((Math.random(i) * max + min)));
+      const randomNum = Math.floor((Math.random(i) * max + min));
+      numbers.push(randomNum);
     }
     return numbers;
   }
 console.log(getRandomArray(14, 1, 10))
 
 
-// 2
+// 2 мода всіх переданих аргументів
 
-  function getModa(arrOfNums) {
+  function getModa(...numbers) {
     const objOfNums = {};
+    const arrOfNums = [...numbers]. filter(n => Number.isInteger(n));
     
-    for (let i = 0; i < arrOfNums.length; i++) {
-      if (Number.isInteger(objOfNums[arrOfNums[i]])) {
-            objOfNums[arrOfNums[i]] += 1
+    arrOfNums.forEach((el) => {
+      if (objOfNums[el]) {
+        objOfNums[el] += 1
       } else {
-            objOfNums[arrOfNums[i]] = 1
-        }
-    }
+        objOfNums[el] = 1
+      }
+    });
     let highestValue = 0;
     let highestValueKey = -Infinity;
-
+    
     for (let key in objOfNums) {
-    const value = objOfNums[key] 
-
-    if (value > highestValue) {
-            highestValue = value;
-            highestValueKey = key;
-     } 
+      const value = objOfNums[key]
+      if (value > highestValue) {
+        highestValue = value;
+        highestValueKey = key;
+      }
     }
- return highestValueKey
+    return highestValueKey
   }
-console.log('mode: ' + getModa(getRandomArray(14, 1, 10)))
+console.log('mode: ' + getModa(1, 5, 3, 6.5, 8, 8.7, 8, 8, 44, 3))
 
 
-// 3
+// 3  середнє арифметичне всіх переданих аргументів
 
-  function getAverage(arrOfNums) {
+  function getAverage(...numbers) {
     let i = 0;
     let averageNum;
-
-    if (Number.isInteger(arrOfNums[i])) {
+    const arrOfNums = [...numbers]. filter(n => Number.isInteger(n));
     let sumOfNums = arrOfNums.reduce((a, b) => a + b, 0); 
     averageNum = sumOfNums / arrOfNums.length;
-      }
+    
     return averageNum
-    }
+  }
 
-console.log('average number: ' +  getAverage(getRandomArray(14, 1, 10)))
+console.log('average number: ' +  getAverage(2, 5, 3, 7, 8, 35, 2))
 
 
-// 4
+// 4 медіана всіх переданих аргументів
 
-  function getMedian(arrOfNums) {
+  function getMedian(...numbers) {
     let i = 0;
-      if (Number.isInteger(arrOfNums[i])) {
-    let sortedArr = arrOfNums.sort(function(a, b){return a - b});
+    const arrOfNums = [...numbers]. filter(n => Number.isInteger(n));
+    let sortedArr = arrOfNums.sort((a, b) => a - b);
     let len = sortedArr.length;
     let median;
-    let medianIndex = Math.floor(len / 2);
+    let middle = Math.floor(len / 2);
+    let thisArrMiddle = arrOfNums[middle]
+    let thisArrRest = arrOfNums[middle - 1]
 
       if (len % 2 === 0) { 
-        median = (arrOfNums[medianIndex] + arrOfNums[medianIndex - 1] ) / 2; 
+        median = (thisArrMiddle + thisArrRest) / 2; 
       } else {
-        median = arrOfNums[medianIndex] 
+        median = arrOfNums[middle] 
       }
       return median      
-}}
-console.log('median: ' + getMedian(getRandomArray(14, 1, 10)))
+}
+console.log('median: ' + getMedian(43, 7, 25, 9, 8, 67, 14, 3))
 
 
-// 6 
+// 6 кількість чисел більших 0
 
-  function countPositiveNumbers(...arrOfNums) {
+  function countPositiveNumbers(...numbers) {
     let positiveNums = [];
     let finalCount;
+    let arr = [...numbers];
 
-    for (let i = 0; i < arrOfNums.length; i++) {
-       if (arrOfNums[i] >= 0) {
-        positiveNums.push(arrOfNums[i])
-       }
-       finalCount = positiveNums.length
-    }
-  return finalCount
-  }
-console.log('count of positive numbers: ' + countPositiveNumbers(-2, 3, 6, -6, 4, -7, 3, -9))
-
-
-// 7
-  function getDividedByFive(arrOfNums) {
-    const dividedByFiveNums = arrOfNums.filter(filterArr);
-
-    const filterArr = value => value % 5 === 0;
+    arr.forEach((el) => {
+      if (el >= 0) {
+        positiveNums.push(el)
+      }
+    });
+    finalCount = positiveNums.length
     
-    // function filterArr(value) {
-    //   return value % 5 === 0;
-    // }
-  return dividedByFiveNums
+    return finalCount
+  }
+       
+console.log('count of positive numbers: ' + countPositiveNumbers(-2, 6, 34, 6, -6, -7, 3, -9))
+
+
+// 7 елементи, які діляться націло на 5
+
+  function getDividedByFive(...numbers) {
+    const filterArr = value => value % 5 === 0;
+    const dividedByFiveNums = [...numbers].filter(filterArr);
+    return dividedByFiveNums
   }
 
-console.log('divided by five: ' + getDividedByFive(getRandomArray(14, 1, 10)))
+console.log('divided by five: ' + getDividedByFive(5, 2, 7, 6, 9, 15))
