a838aec2d5f8800e4b86fba645a05da7f0406bde
Anastasia Novosyolova <anastasia@Anastasias-MacBook-Air.local>
2022-09-21 15:47:12 +0300

    fixed

--- homework-06/homework-06.js
+++ homework-06/homework-06.js
@@ -28,15 +28,14 @@ const students = [{
 // 1 список предметів для конкретного студента
 
   function getSubjects(obj) {
-    const replacedUnderscore = [];
     const subjArr = Object.keys(obj.subjects);
-
-      for(let i = 0 ; i < subjArr.length ; i++){
-        replacedUnderscore[i] = (subjArr[i].charAt(0).toUpperCase() + subjArr[i].slice(1)).replace(/_/g, " ");
-      }  
-  return replacedUnderscore
+    let subj = subjArr.map(function(el) {
+      const firstLetter = el.charAt(0).toUpperCase();
+      const withoutFirstLetter = el.slice(1);
+      return firstLetter + withoutFirstLetter.replace(/_/g, " ");
+    });
+    return subj
   }
-
   console.log(getSubjects(students[0]))
 
 
@@ -44,14 +43,8 @@ const students = [{
 
 
   function getAverageMark(obj) {
-    let marks = [];
-  
-    marks = Object.values(obj.subjects)
-
-    const mergedMarks = marks.reduce((total, amount) => {
-    return total.concat(amount);
-    }, []);
-  
+    let marks = Object.values(obj.subjects)
+    const mergedMarks = marks.flat()
     let sumOfMarks = mergedMarks.reduce((a, b) => a + b, 0); 
     let averageMark = sumOfMarks / mergedMarks.length;
   
@@ -63,11 +56,15 @@ console.log(getAverageMark(students[0]))
 
 // 3 інформація загального виду по переданому студенту
 
-  function getStudentInfo(obj) {
+// function getStudentInfo({name, course}) {  }
+// function displaySummary({ name, scores: { maths = 0, english = 0, science = 0 } })
+
+
+  function getStudentInfo({name, course}) {
 
     let studentsInfo = {
-      name: obj.name,
-      course: obj.course,
+      name,
+      course,
       averageMark: getAverageMark(obj)
     }
   return studentsInfo
@@ -76,16 +73,12 @@ console.log(getAverageMark(students[0]))
 console.log(getStudentInfo(students[0]))
 
 
+
 // 4 імена студентів у алфавітному порядку
 
   function getStudentsNames(arr) {
-    const arrOfNames = [];
-
-    for(let student of arr) {
-    arrOfNames.push(student.name)
-  
-    }
-  return arrOfNames.sort()
+    const arrOfNames = arr.map((el) => el.name)
+    return arrOfNames.sort()
   }
 
 console.log(getStudentsNames(students))
@@ -97,7 +90,7 @@ console.log(getStudentsNames(students))
     const allAverageMarks = [];
 
     for(let i = 0; i < arr.length; i++) {
-    allAverageMarks.push(Number(getAverageMark(arr[i])))
+      allAverageMarks.push(Number(getAverageMark(arr[i])))
     }
     indexOfMaxMark = allAverageMarks.indexOf(Math.max(...allAverageMarks))
     return arr[indexOfMaxMark].name
@@ -112,8 +105,8 @@ console.log(getBestStudent(students))
     const arrOfLetters = str.split('')
     const count = {};
 
-    arrOfLetters.forEach(function (i) { 
-    count[i] = (count[i] || 0) + 1; 
+    arrOfLetters.forEach(function(i) {
+      count[i] = (count[i] || 0) + 1; 
     });
   return count
   
--- homework-07/homework-07.js
+++ homework-07/homework-07.js
@@ -7,7 +7,6 @@ const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 // 1 скільки податків ви заплатите як IT-спеціаліст в якійсь з країн
 
   function getMyTaxes(salary) {
-
     return this.tax * salary; 
   }
 
@@ -17,7 +16,7 @@ console.log(getMyTaxes.call(litva, 1000))
 // 2 скільки у середньому податків платятт IT-спеціалісти у кожній країні
 
   function getMiddleTaxes() {
-    return (this.tax * this.middleSalary)
+    return this.tax * this.middleSalary
   }
 
 console.log(getMiddleTaxes.call(litva))
@@ -26,7 +25,7 @@ console.log(getMiddleTaxes.call(litva))
 // 3 скільки всього податків платять IT-спеціалісти у кожній країні
 
   function getTotalTaxes() {
-    return (this.tax * this.middleSalary * this.vacancies)
+    return this.tax * this.middleSalary * this.vacancies
   }
 
   console.log(getTotalTaxes.call(ukraine))
@@ -37,7 +36,9 @@ console.log(getMiddleTaxes.call(litva))
     function getMySalary(country) {
       let timer = 0;
       let timerId = setInterval(() => {
-        const salary = Math.floor(Math.random() * (2000 - 1500)) + 1500;
+        minSalary = 1500;
+        maxSalary = 2000;
+        const salary = getRandomNumber();
         const tax = +getMyTaxes.call(country, salary).toFixed(2);
         const profit = +(salary - tax).toFixed(2);
         console.log({ 
@@ -45,10 +46,13 @@ console.log(getMiddleTaxes.call(litva))
           taxes: tax, 
           profit: profit 
           })
-        if (timer === 10) {
-          clearInterval(timerId);
-        }
-        timer++
-      }, 5000)
+          if (timer === 10) {
+            clearInterval(timerId);
+          }
+          timer++
+        }, 10000)
+        getRandomNumber = () => Math.floor(Math.random() * (maxSalary - minSalary)) + minSalary;
     }
     getMySalary(ukraine)
+
+    
