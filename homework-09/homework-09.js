7f16925d4ee63ff45cf7a5a6b9c744abcb3012ca
Anastasia Novosyolova <anastasia@Anastasias-MacBook-Air.local>
2022-09-22 12:34:19 +0300

    fixed

--- homework-09/homework-09.js
+++ homework-09/homework-09.js
@@ -9,35 +9,25 @@ const grid = document.querySelector('.grid')
       const div = document.createElement('div');
       div.classList.add('block');
       grid.append(div)
-    }
+    } 
   }
   createDivElement()
 
 
-// Рандомний колір
 
-  const randomColor = () => {
-    let randomNum = (Math.random() * 0xfffff * 1000000).toString(16);
-    return '#' + randomNum.slice(0, 6);
-  };
-
-
-// Щоб квадрати з'явились на сторінці, generateBlocks();
-
-  function generateBlocks() {
-    let boxes = document.querySelectorAll('.block');
-    boxes.forEach((el) => el.style.background = randomColor())
-  }
-  generateBlocks()
-
-
-// Щоб квадрати з'явились на сторінці та почали змінюватись, generateBlocksInterval(); 
 
   function generateBlocksInterval() {
-    let timer = 0;
+    function generateBlocks() {
+      const randomColor = () => {
+        let randomNum = (Math.random() * 0xfffff * 1000000).toString(16);
+        return '#' + randomNum.slice(0, 6);
+      };
+      let boxes = document.querySelectorAll('.block');
+      boxes.forEach((el) => el.style.background = randomColor())
+    };
     setInterval(() => {
       generateBlocks()
-      timer++
     }, 1000)
   }
   generateBlocksInterval()
+
