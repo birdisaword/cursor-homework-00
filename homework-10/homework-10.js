f5c4f8e260648adae5548feff21e17bb56168809
Anastasia Novosyolova <anastasia@Anastasias-MacBook-Air.local>
2022-09-27 19:41:29 +0300

    fixed

--- homework-10/homework-10.js
+++ homework-10/homework-10.js
@@ -1,22 +1,22 @@
 
 const keys = document.querySelectorAll('.key');
 
-function playNote(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`),
-          key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-
-    if(!key) return;
-
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
+function playSound(e) {
+  const sound = document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
+  if(!key) {
+    return
+  }
+  sound.currentTime = 0;
+  sound.play();
 }
 
-function removeTransition(e){
-    if(e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') {
+    return
+  }
 }
 
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
-window.addEventListener('keydown', playNote);
+window.addEventListener('keydown', playSound);
