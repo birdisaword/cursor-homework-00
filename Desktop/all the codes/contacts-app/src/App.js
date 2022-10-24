b0ab46753f0f7213fd2ca782d6c1e5e8f7b36ecc
birdisaword <knockmedown89@protonmail.com>
2022-10-24 16:03:10 +0300

    fixed

---
+++ Desktop/all the codes/contacts-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Contact from './AppComponent'
+
+function App() {
+
+  return (
+    <div className="App">
+         <Contact />  
+    </div>
+  );	
+}
+
+export default App;
+
+
